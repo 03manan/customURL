21c71eaac384c3176a241f9aca7490fac68e114b
Manan373 <118926720+Manan373@users.noreply.github.com>
2024-08-08 17:48:29 +0530

    Latest deployement changes

--- db/connectDB.js
+++ db/connectDB.js
@@ -1,12 +1,13 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const connectToMongoDB = async () => {
     try {
-        await mongoose.connect("mongodb+srv://oxymanan160:1234@cluster0.oz0pei9.mongodb.net/customURL");
-        console.log("Connected to MongoDB.")
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log('Connected to MongoDB');
     } catch (error) {
-        console.log("Mongo DB connection error:", error)
+        console.error('Error connecting to MongoDB:', error);
+        process.exit(1);
     }
-}
+};
 
 module.exports = connectToMongoDB;
--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require("express")
 const urlRoute = require("./routes/url.routes.js");
 const  connectToMongoDB  = require("./db/connectDB.js");
 const cors = require('cors');
+require('dotenv').config();
 
 
 const app = express()
-const allowedOrigins = ['http://localhost:5500', 'http://127.0.0.1:5500'];
+// const allowedOrigins = ['http://localhost:5500', 'http://127.0.0.1:5500'];
+const allowedOrigins = ['https://customurl-9d2h.onrender.com'];
 // app.use(cors({
 //     origin: 'http://localhost:5500'  // Allow only requests from this origin
 //   }));
@@ -36,3 +38,37 @@ app.use("/api", urlRoute)
 app.listen(PORT, () => {
     console.log(`Server se connection ho gya hai on port: ${PORT}`)
 } )
+
+
+// for deployement:
+// require('dotenv').config();
+// const express = require('express');
+// const urlRoute = require('./routes/url.routes.js');
+// const connectToMongoDB = require('./db/connectDB.js');
+// const cors = require('cors');
+
+// const app = express();
+
+// // Enable CORS
+// app.use(cors({
+//     origin: process.env.CORS_ORIGIN
+// }));
+
+// // Middleware to parse JSON
+// app.use(express.json());
+
+// // Establish Database Connection
+// connectToMongoDB();
+
+// // Setup Routes
+// app.use('/api', urlRoute);
+
+// // Serve static files from the "client" directory
+// app.use(express.static('client'));
+
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//     console.log(`Server is connected on port: ${PORT}`);
+// });
+
+// module.exports = app;

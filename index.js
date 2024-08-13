e5f57fd59dcaa3a4f2f15f39e9bfbad1bae04c3a
Manan373 <118926720+Manan373@users.noreply.github.com>
2024-08-13 16:47:38 +0530

    readme updated

--- index.js
+++ index.js
@@ -11,17 +11,25 @@ const app = express()
 // app.use(cors({
 //     origin: 'process.env.CORS_ORIGIN_DEV'  // Allow only requests from this origin
 //   }));
-const allowedOrigins = process.env.CORS_ORIGIN_DEV.split(',');
-app.use(cors({
-    origin: function (origin, callback) {
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.indexOf(origin) === -1) {
-            const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-            return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-    }
-}));
+
+
+//deployement cors
+// const allowedOrigins = process.env.CORS_ORIGIN_DEV.split(',');
+// app.use(cors({
+//     origin: function (origin, callback) {
+//         if (!origin) return callback(null, true);
+//         if (allowedOrigins.indexOf(origin) === -1) {
+//             const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+//             return callback(new Error(msg), false);
+            
+
+//         }
+//         return callback(null, true);
+//     }
+// }));
+//deployement cors
+
+app.use(cors());
 
 //middleware
 app.use(express.json());
